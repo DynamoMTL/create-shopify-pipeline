1b328a3d0d11de0062d761867541811ba4d5a120
Nicolas Duvieusart Déry <nddery@gmail.com>
2017-06-10 13:01:44 -0400

    Work with a temporary directory until everything is correctly installed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable newline-per-chained-call */
 /**
  * This is installed as a global package. Do not edit. Getting user's to update
  * their global package is a pain.
@@ -13,6 +14,7 @@
 const { execSync, spawn } = require('child_process')
 const fs = require('fs')
 const path = require('path')
+const tmp = require('tmp')
 const validateNpmPackageName = require('validate-npm-package-name')
 
 const colors = {
@@ -23,14 +25,9 @@ const colors = {
 }
 
 const dirname = ((n) => {
-  if (!n) {
-    console.log(`${colors.red}ERROR: Please provide your desired application name as the first and only argument.`)
-    process.exit(1)
-  }
-
   const validation = validateNpmPackageName(n)
   if (validation.errors) {
-    console.log(`${colors.red}ERROR: The provided application name is invalid: ${validation.errors.join(', ')}`)
+    console.log(`${colors.red}The provided application name is invalid: ${validation.errors.join(', ')}`)
     process.exit(1)
   }
 
@@ -38,6 +35,8 @@ const dirname = ((n) => {
 })(process.argv[2])
 
 const dirpath = path.resolve(dirname)
+const tmpdirObj = tmp.dirSync()
+const tmpdir = tmpdirObj.name
 
 // Return the package name to use, allow overwrite by using
 // `create-shopify-pipeline MyShopifyTheme --interal-testing-repo=git+ssh://git@github.com:DynamoMTL/create-shopify-pipeline`
@@ -52,11 +51,24 @@ const pkg = (() => {
 // Return the package name, even if the package is a git+ssh URL
 const pkgName = ((p) => {
   if (p.startsWith('git+ssh')) {
-    return p.split('#').shift().split('/').pop().split('.').shift()
+    return p.split('#').shift() // version or branch name is passed after a hash
+      .split('/').pop() // package name, with .git extension
+      .split('.').shift() // just the package name
   }
 
   return p
 })(pkg)
+
+const yarnAvailable = (() => {
+  try {
+    // Use `yarnpkg` so we're sure we don't spin up Hadoop' own yarn by mistake...
+    // https://github.com/yarnpkg/yarn/issues/673
+    execSync('yarnpkg --version', { stdio: 'ignore' })
+    return true
+  } catch (e) {
+    return false
+  }
+})()
 
 /**
  * Validation the given path does not yet exists and that the current user
@@ -100,22 +112,6 @@ function writePackageJson(root, packageName) {
 }
 
 /**
- * Check if Yarn is installed.
- *
- * @return Boolean
- */
-function useYarn() {
-  try {
-    // Use `yarnpkg` so we're sure we don't spin up Hadoop' own yarn by mistake...
-    // https://github.com/yarnpkg/yarn/issues/673
-    execSync('yarnpkg --version', { stdio: 'ignore' })
-    return true
-  } catch (e) {
-    return false
-  }
-}
-
-/**
  * Install package with yarn/npm
  *
  * @param   String  root  The path in which to run the install
@@ -129,12 +125,12 @@ function install(dep, root) {
     let command = 'yarnpkg'
     let args = ['add', '--dev', dep]
 
-    if (!useYarn()) {
+    if (!yarnAvailable) {
       command = 'npm'
       args = ['install', '--save-dev', dep]
     }
 
-    const process = spawn(command, args, { cwd: root, stdio: 'ignore' })
+    const process = spawn(command, args, { cwd: root, stdio: 'inherit' })
 
     process.on('close', (code) => {
       if (code !== 0) {
@@ -154,7 +150,7 @@ function install(dep, root) {
  * @return  void
  */
 function runPackageInit(root, packageName) {
-  console.log('Bootstraping your theme.')
+  console.log('\nBootstraping...')
   const initScript = path.resolve(
     root,
     'node_modules',
@@ -167,33 +163,56 @@ function runPackageInit(root, packageName) {
   require(initScript)(root)
 }
 
-// @TODO write to tmp directory and move it afterwards ?
-validateDirectory(dirpath)
-  .then(() => fs.mkdirSync(dirpath))
-  .then(console.log(`Creating a new Shopify Pipeline theme in ${colors.green}${dirpath}${colors.reset}`))
-  .then(() => writePackageJson(dirpath, dirname))
-  .then(() => install(pkg, dirpath))
-  .then(() => runPackageInit(dirpath, pkgName))
-  .then(() => {
-    const hasYarn = useYarn()
-    const command = hasYarn ? 'yarn' : 'npm run'
+/**
+ * Move a directory
+ *
+ * @param   String  oldPath   Path of the directory to move
+ * @param   String  newPath   Path of the new/destination directory
+ * @return  {Promise.<String, Error>}
+ */
+function move(oldPath, newPath) {
+  return new Promise((resolve, reject) => {
+    fs.rename(oldPath, newPath, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(newPath)
+      }
+    })
+  })
+}
 
-    console.log(`${colors.green}
-${colors.green}Success!${colors.reset} You're now ready to write yourself an amazing Shopify theme.
+function printSuccessMessage() {
+  const command = yarnAvailable ? 'yarn' : 'npm run'
+
+  console.log(`${colors.green}
+Success!${colors.reset} You're now ready to write yourself an amazing Shopify theme.
 
 Don't forget to fill out \`config/shopify.yml\`.
 
 Some commands you'll most likely use:
-  ${colors.cyan}${command} serve${colors.reset}\tStart the development server, upload files as they change
-  ${colors.cyan}${command} build${colors.reset}\tBuild the theme in the \`dist\` folder
+  ${colors.cyan}${command} serve ${colors.reset}\tStart the development server, upload files as they change
+  ${colors.cyan}${command} build ${colors.reset}\tBuild the theme in the \`dist\` folder
   ${colors.cyan}${command} deploy${colors.reset}\tBuild and deploy your theme
-  ${colors.cyan}${command} test${colors.reset}\tRun your tests
-    `)
-  })
+  ${colors.cyan}${command} test  ${colors.reset}\tRun your tests
+  `)
+}
+
+function printErrorMessage(err) {
+  console.log(`${colors.red}
+An error occurred:
+${err}
+  `)
+}
+
+validateDirectory(dirpath)
+  .then(console.log(`Creating a new Shopify Pipeline theme in ${colors.green}${dirpath}${colors.reset}`))
+  .then(() => writePackageJson(tmpdir, dirname))
+  .then(() => install(pkg, tmpdir))
+  .then(() => runPackageInit(tmpdir, pkgName))
+  .then(() => move(tmpdir, dirpath))
+  .then(printSuccessMessage)
   .catch((err) => {
-    console.log(`${colors.red}
-      An error occurred:
-      ${err}
-    `)
+    printErrorMessage(err)
     process.exit(1)
   })
